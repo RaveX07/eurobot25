4edf7f9d2286ef3eed600ae98c174e39941d222b
Jannis Schröter <73310649+RaveX07@users.noreply.github.com>
2024-10-26 16:55:57 +0200

    add odometry

--- teensy_main/src/main.cpp
+++ teensy_main/src/main.cpp
@@ -5,11 +5,24 @@
 #define ENC_RIGHT_A_PHASE 18
 #define ENC_RIGHT_B_PHASE 19
 
+#define WHEELBASE_ENC 127
+#define WHEELBASE_DRIVE 207
+#define PULSES_PER_REV 1200
+#define ENC_WHEEL_DIAMETER 50                                                                           
+#define MOTOR_WHEEL_DIAMETER 70
+
+const float encWheelScope = ENC_WHEEL_DIAMETER * M_PI;
+const float pulsesPerMM = PULSES_PER_REV / encWheelScope;
+
 int counterLeft = 0;
 int counterRight = 0;
 
-int loops = 0;
-long lastMillis = millis();
+int lastCounterLeft = 0;
+int lastCounterRight = 0;
+
+float x = 0;
+float y = 0;
+float theta = 0;
 
 void changeLeft(){
     if(digitalRead(ENC_LEFT_B_PHASE) != digitalRead(ENC_LEFT_A_PHASE)){
@@ -27,6 +40,44 @@ void changeRight(){
     }
 }
 
+void updatePos(){
+    // get tick differnce for each wheel
+    float leftWheelDif = counterLeft - lastCounterLeft;
+    float rightWheelDif = counterRight - lastCounterRight;
+
+    // update last pos
+    lastCounterLeft = counterLeft;
+    lastCounterRight = counterRight;
+
+    // distance per wheel
+    float distanceLeft = leftWheelDif / pulsesPerMM;
+    float distanceRight = rightWheelDif / pulsesPerMM;
+
+    float distance = (distanceLeft + distanceRight) / 2;
+
+    // global position change
+    float dtheta = (distanceLeft - distanceRight) / WHEELBASE_ENC;
+    float dx = distance * cos(theta + dtheta);
+    float dy = distance * sin(theta + dtheta);
+    
+
+    // update global pos
+    x = x + dx;
+    y = y + dy;
+    theta = theta + dtheta;
+    while (theta > 2 * M_PI) theta -= 2 * M_PI;
+    while (theta < -2 * M_PI) theta += 2 * M_PI;
+
+    Serial.print("X: ");
+    Serial.println(x);
+    Serial.print("Y: ");
+    Serial.println(y);
+    Serial.print("Theta: ");
+    Serial.println(theta * (180.0 / M_PI));
+
+    Serial.println("####################");
+}
+
 void setup() {
     Serial.begin(115200);
 
@@ -39,10 +90,6 @@ void setup() {
 }
 
 void loop() {
-    Serial.print("Counter Left: ");
-    Serial.println(counterLeft);
-    Serial.print("Counter Right: ");
-    Serial.println(counterRight);
-
-    delay(100);
+    updatePos();
+    delay(10);
 }
