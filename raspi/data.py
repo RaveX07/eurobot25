c05c110750fc68ebcb77f569800261af018682b5
RaveX07 <jannis.schroeter@gmx.de>
2024-11-30 22:15:17 +0100

    optimize raspi code

--- raspi/data.py
+++ raspi/data.py
@@ -1,39 +1,49 @@
 import serial
 import re
 import time
+from position import Position
 
-ser = serial.Serial("/dev/ttyACM0", 115200, timeout=1)
+class SerialManager():
+    def __init__(self, port="/dev/ttyACM0", baud_rate=115200):
+        self.ser = serial.Serial(port, baud_rate, timeout=1)
+        
+        self.ser.setDTR(False)
+        time.sleep(1)
+        self.ser.flushInput()
+        self.ser.setDTR(True)
+        time.sleep(2)
 
-ser.setDTR(False)
-time.sleep(1)
-ser.flushInput()
-ser.setDTR(True)
-time.sleep(2)
-
-def extract_values(s):
-    # Find matches for x, y, and t as floats
-    x_match = re.search(r'x([-+]?\d+\.\d+)', s)
-    y_match = re.search(r'y([-+]?\d+\.\d+)', s)
-    t_match = re.search(r't([-+]?\d+\.\d+)', s)
+    # serial string to x, y, z
+    def extract_values(self, s):
+        # Find matches for x, y, and t as floats
+        x_match = re.search(r'x([-+]?\d+\.\d+)', s)
+        y_match = re.search(r'y([-+]?\d+\.\d+)', s)
+        t_match = re.search(r't([-+]?\d+\.\d+)', s)
+        
+        # Check if all matches are found
+        if not (x_match and y_match and t_match):
+            raise ValueError(f"Could not extract all values from string: {s}")
+        
+        # Extract and convert to floats
+        x = float(x_match.group(1))
+        y = float(y_match.group(1))
+        t = float(t_match.group(1))
+        
+        return x, y, t
     
-    # Check if all matches are found
-    if not (x_match and y_match and t_match):
-        raise ValueError(f"Could not extract all values from string: {s}")
-    
-    # Extract and convert to floats
-    x = float(x_match.group(1))
-    y = float(y_match.group(1))
-    t = float(t_match.group(1))
-    
-    return x, y, t
+    def get_pos(self):
+        self.ser.flushInput()
+        
+        while True:
+            line = self.ser.readline()
+            if line:
+                x, y, t = self.extract_values(line.decode("utf-8"))
+                break
 
+        print(f'Arduino sent: x:{x}, y:{y}, t:{t}')
+        return Position(x, y, t)
+
+manager = SerialManager()
 while True:
-    ser.flushInput()
-	# read to get the acknowledgement from the Arduino
-    while True:
-        line = ser.readline()
-        if line:
-            x, y , t = extract_values(line.decode("utf-8"))
-            break
-
-    print(f'Arduino sent: x:{x}, y:{y}, t:{t}')
+    manager.get_pos()
+    time.sleep(1)
--- raspi/pathfinding.py
+++ raspi/pathfinding.py
@@ -3,6 +3,7 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 from heapq import heappop, heappush
+from position import Position
 
 def add_with_limits(a, b, max_value):
     result = a + b
@@ -11,27 +12,6 @@ def add_with_limits(a, b, max_value):
     elif result < 0:
         return 0
     return result
-
-
-
-class Position():
-    def __init__(self, x, y, theta=0):
-        self.x = x
-        self.y = y
-        self.theta = theta
-        
-        self.limit()
-        
-    def limit(self):
-        if(self.x > 2999):
-            self.x = 2999
-        elif(self.x < 0):
-            self.x = 0
-        
-        if(self.y > 1999):
-            self.y = 1999
-        elif(self.y < 0):
-            self.y = 0
             
 
 class Pathfinder():
