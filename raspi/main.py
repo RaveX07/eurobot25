79626f26f67e47bfeb9537a24f14de54b666682e
Jannis Schröter <73310649+RaveX07@users.noreply.github.com>
2024-12-02 15:42:32 +0100

    test multithreading

--- raspi/main.py
+++ raspi/main.py
@@ -1,13 +1,27 @@
 from data import SerialManager
 from position import Position
 from time import sleep
+from threading import Thread
 
 pos = Position(0, 0)
 
-if __name__ == '__main__':
+pwm_left = (0, 0)
+pwm_right = (0, 0)
+
+def serial_process():
     serial_manager = SerialManager()
     while True:
+        global pos
         pos = serial_manager.get_pos()
-        serial_manager.send_pwm((255, 0), (255, 0))
-        print("sent pwm")
+        serial_manager.send_pwm(pwm_left, pwm_right)
+        print(f"sent pwm {pwm_left[0]}, {pwm_left[1]}, {pwm_right[0]}, {pwm_right[1]}")
         sleep(0.1)
+        
+serial_thread= Thread(target=serial_process, args=())
+
+if __name__ == '__main__':
+    serial_thread.start()
+    
+while True:
+    pwm_left[0:1] += 1
+    pwm_right[0:1] += 1
